6d8b58529ab2431a8b5125c5e59cf60f4568d839
Ícaro Chagas <icaro.chagas.a@gmail.com>
2021-06-14 01:46:57 -0300

    Consulta simples com query methods

--- src/main/java/com/educandoweb/workshopmongo/services/PostService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.educandoweb.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
